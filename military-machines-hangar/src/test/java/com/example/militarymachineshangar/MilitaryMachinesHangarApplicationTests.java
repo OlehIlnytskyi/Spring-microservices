eaf3c64c8bcf2fa98e9f7743334a7196ded140d6
Oleg Ilnystkyi <oleh.ilnytskyi.knm.2020@lpnu.ua>
2023-11-15 20:28:49 +0200

    Added client Military Machines Hangar

---
+++ military-machines-hangar/src/test/java/com/example/militarymachineshangar/MilitaryMachinesHangarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.militarymachineshangar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilitaryMachinesHangarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

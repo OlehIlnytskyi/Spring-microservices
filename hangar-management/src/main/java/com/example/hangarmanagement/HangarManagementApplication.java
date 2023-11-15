7ecd644a733dd8ba50997388efc6d4677fa73316
Oleg Ilnystkyi <oleh.ilnytskyi.knm.2020@lpnu.ua>
2023-11-15 20:34:49 +0200

    Added Client Hangar Management

---
+++ hangar-management/src/main/java/com/example/hangarmanagement/HangarManagementApplication.java
@@ -0,0 +1,15 @@
+package com.example.hangarmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class HangarManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HangarManagementApplication.class, args);
+	}
+
+}
---
+++ hangar-management/src/test/java/com/example/hangarmanagement/HangarManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hangarmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HangarManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
